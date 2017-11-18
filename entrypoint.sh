ad3563ffa579a34dcce51885c6a223fa6541e933
yulahuyed <yulahuyed@vpsorg.top>
2017-11-18 09:08:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+#start net speeder
+ETH=$(eval "ifconfig | grep 'eth0'| wc -l")
+if [ "$ETH"  ==  '1' ] ; then
+	nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
+fi
+if [ "$ETH"  ==  '0' ] ; then
+    nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
+fi
+
  /goproxy-vps/goproxy-vps $CONFIG_FILE_URL -logtostderr -v 3
